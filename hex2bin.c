c12b5e6d9acb5895dfba33e56c7b36b646f920fa
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-07-07 16:18:38 -0400

    Clean up formatting.

--- hex2bin.c
+++ hex2bin.c
@@ -37,30 +37,30 @@ static void str2upper(char *s);
 // hex2bin:  convert a 2 digit hex string into an 8 digit binary string
 int main(int argc, char *argv[]) {
 
-	unsigned char res;
-	unsigned char bin[9];
-	int len = 0;
-	char hex[8];
-	char *pin = NULL;
+    unsigned char res;
+    unsigned char bin[9];
+    int len = 0;
+    char hex[8];
+    char *pin = NULL;
 
-	if (argc != 2) exit(EXIT_FAILURE);
+    if (argc != 2) exit(EXIT_FAILURE);
 
-	len = strlen(argv[1]);
-		pin = argv[1];
-	if (len == 4) pin += 2;
+    len = strlen(argv[1]);
+        pin = argv[1];
+    if (len == 4) pin += 2;
 
-	strncpy(hex, pin, 2);
-	str2upper(hex);
-	memset(bin, 0, 9);
-	res = htoi(hex);
+    strncpy(hex, pin, 2);
+    str2upper(hex);
+    memset(bin, 0, 9);
+    res = htoi(hex);
 #if DEBUG
-	printf("main: \tfirst char: %c\n", argv[1][0]);
-	printf("main: \tfirst char: %c\n", hex);
-	printf("main: \tinput = %u\n", (unsigned int)res);
+    printf("main: \tfirst char: %c\n", argv[1][0]);
+    printf("main: \tfirst char: %c\n", hex);
+    printf("main: \tinput = %u\n", (unsigned int)res);
 #endif
-	dec2bin(res, bin);
-	printf("%s\n", (char *)bin);
-	return(EXIT_SUCCESS);
+    dec2bin(res, bin);
+    printf("%s\n", (char *)bin);
+    return(EXIT_SUCCESS);
 }
 
 
@@ -68,63 +68,62 @@ int main(int argc, char *argv[]) {
 //        integer
 static unsigned char htoi(char *s) {
 
-	unsigned int i = (unsigned int)EOF;
+    unsigned int i = (unsigned int)EOF;
 
-	// convert low nibble
-	if ((s[1] >= 'A') && (s[1] <= 'F'))
-		i = s[1] - 55;
-	else if ((s[1] >= '0') && s[1] <= '9')
-		i = s[1] - 48;
-	else {
-		return (unsigned char)i;
-	}
+    // convert low nibble
+    if ((s[1] >= 'A') && (s[1] <= 'F'))
+        i = s[1] - 55;
+    else if ((s[1] >= '0') && s[1] <= '9')
+        i = s[1] - 48;
+    else {
+        return (unsigned char)i;
+    }
 
-	// convert high nibble
-	if ((s[0] >= 'A') && (s[0] <= 'F'))
-		i = i + ((s[0] - 55) * 16);
-	else if ((char)toupper(s[0]) >= '0' && (char)toupper(s[0]) <= '9')
-		i = i + ((s[0] - 48) * 16);
-	else {
-		return (unsigned char)i;
-	}
+    // convert high nibble
+    if ((s[0] >= 'A') && (s[0] <= 'F'))
+        i = i + ((s[0] - 55) * 16);
+    else if ((char)toupper(s[0]) >= '0' && (char)toupper(s[0]) <= '9')
+        i = i + ((s[0] - 48) * 16);
+    else {
+        return (unsigned char)i;
+    }
 
-	return (unsigned char)i;
+    return (unsigned char)i;
 }
 
 
 // dec2bin:  convert an unsigned integer to string of binary digits
 static void dec2bin(unsigned char c, unsigned char bin[]) {
 
-	int i = 0;
-	unsigned char lc = c;
+    int i = 0;
+    unsigned char lc = c;
 #ifdef DEBUG
-	printf("dec2bin: c = %d\n", c);
+    printf("dec2bin: c = %d\n", c);
 #endif
-	for (i = 7; i >= 0; i--)  {
-		if ((lc % 2) == (unsigned char)0)
-			bin[i] = '0';
-		else
-			bin[i] = '1';
-		lc >>= 1;
+    for (i = 7; i >= 0; i--)  {
+        if ((lc % 2) == (unsigned char)0)
+            bin[i] = '0';
+        else
+            bin[i] = '1';
+        lc >>= 1;
 #if DEBUG
-		printf("dec2bin: bin[%d] = %d, lc = %u\n", i, bin[i], lc);
+        printf("dec2bin: bin[%d] = %d, lc = %u\n", i, bin[i], lc);
 #endif
-	}
-	bin[8] = '\0';
+    }
+    bin[8] = '\0';
 }
-
 
 
 // str2upper: convert a string to upper case
 static void str2upper(char *s) {
 
-	int i = 0;
-	while (*s != '\0') {
-		*s = toupper(*s);
-		s++;
-		i++;
-	}
+    int i = 0;
+    while (*s != '\0') {
+        *s = toupper(*s);
+        s++;
+        i++;
+    }
 #if DEBUG
-	printf("str2upper: %d\n", i);
+    printf("str2upper: %d\n", i);
 #endif
 }
